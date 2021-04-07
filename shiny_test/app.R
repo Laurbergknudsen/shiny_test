ced2698d6b490e967b4cb1266aba8465167a2d7f
laurbergknudsen <laurberg_knudsen@hotmail.com>
2021-04-07 11:06:43 +0200

    created 2 new tabs + styling datatable

--- shiny_test/app.R
+++ shiny_test/app.R
@@ -11,12 +11,20 @@ library(shiny)
 library(shinythemes)
 library(tidyverse)
 library(readxl)
+library(DT)
+library(bslib)
+library(thematic)
+
+
 
 passat <- read_excel("passat.xlsx")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     theme = shinytheme("darkly"),
+  mainPanel(
+    DTOutput("table")
+  ),
 
     # Application title
     titlePanel(title = div(img(height = 100, src = "logo.jpg")), windowTitle = "EaDania"),
@@ -26,13 +34,13 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(position = "left",
         sidebarPanel( "",
-                      selectInput("xaxis", label = "Vælg x-akse", 
+                      selectInput("xaxis", label = "Vælg x-akse:", 
                                   choices = names(passat), selected = "km_per_liter", 
                                   TRUE, multiple = FALSE),
-                      selectInput("yaxis", label = "Vælg y-akse", 
+                      selectInput("yaxis", label = "Vælg y-akse:", 
                                   choices = names(passat), multiple = FALSE),
-                      helpText("hjælpe tekst"),
-                      selectInput("data_input", label = "Vælg datasæt", choices = c("mtcars","faithful","iris"))
+                      helpText(""),
+                      selectInput("data_input", label = "Vælg datasæt:", choices = c("mtcars","faithful","iris"))
             
            
             ),
@@ -40,8 +48,23 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel("",
                   tabsetPanel(id = "tabs",
-                              tabPanel("Plot", plotOutput("plot1",width = "100%")),
-                              tabPanel("Tabel", tableOutput("tabel1")))
+                              tabPanel("Bil Priser",
+                                       sliderInput(inputId = "slider_pris", 
+                                                   label = "Vælg pris", min = floor(min(passat$price)/1000)*1000,
+                                                   max = ceiling(max(passat$price)/1000)*1000,
+                                                   value = c(floor(min(passat$price)/1000)*1000,ceiling(max(passat$price)/1000)*1000), step = 1000,
+                                                   round = TRUE, ticks = TRUE),
+                                       dataTableOutput("price_table")),
+                              tabPanel("Bil specs",
+                                       selectizeInput(inputId = "add_to_table",
+                                                      label = "Tilføj til tabel",
+                                                      choices = passat$car,
+                                                      multiple = FALSE),
+                                       dataTableOutput("usertable")),
+                              tabPanel("Plot",
+                                       plotOutput("plot1",width = "100%")),
+                              tabPanel("Tabel",
+                                       tableOutput("tabel1")))
                               
                   )
     )
@@ -49,6 +72,8 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  
+  
 
     output$plot1 <- renderPlot({
         ggplot(passat,aes_string(x = as.name(input$xaxis), y = as.name(input$yaxis))) +
@@ -59,8 +84,38 @@ server <- function(input, output, session) {
         get(input$data_input, "package:datasets")
     })
     
-    output$tabel1 <-  renderTable({head(getdata())})
+    output$tabel1 <-  renderTable({head(getdata(),5)})
     
+   df_add_car <- reactive({
+     
+     req(input$add_to_table)
+     
+     df_add_car <-  passat %>% 
+       filter(car == input$add_to_table)
+     
+   })
+    
+    output$usertable <- renderDataTable({
+      datatable(df_add_car(), style = "bootstrap")
+    })
+    
+
+    df_biler <- reactive({
+
+      req(input$slider_pris)
+
+      df_biler <-  passat %>%
+        filter(price >= input$slider_pris[1], price <= input$slider_pris[2])
+
+    })
+
+    output$price_table <- renderDataTable({
+     datatable(df_biler(), style = "bootstrap")
+      
+      
+    })
+
+
     
 }
 
