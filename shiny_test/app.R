dca8bf88f84b77333a0a77532efd3e4d2721088e
Laurbergknudsen <laurberg_knudsen@hotmail.com>
2021-05-18 12:22:33 +0200

    This wont work, needs goggle auth and a sheet

--- shiny_test/app.R
+++ shiny_test/app.R
@@ -14,13 +14,28 @@ library(readxl)
 library(DT)
 library(bslib)
 library(thematic)
+library(shinyWidgets)
+library(googlesheets4)
+library(shinyTime)
 
-
+test = data.frame(chaufførid = as.integer(),
+                  typedag = as.character(),
+                  rute = as.character(),
+                  temperatur = as.integer(),
+                  batteri = as.double(),
+                  tid = as.numeric(),
+                  lagToStart = as.numeric(),
+                  check.names = FALSE)
 
 passat <- read_excel("passat.xlsx")
 
 minPrice <- floor(min(passat$price)/1000)*1000
 maxPrice <- ceiling(max(passat$price)/1000)*1000
+
+gs4_auth(
+  # cache = ".secrets",
+  # email = "*"
+)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -30,33 +45,74 @@ ui <- fluidPage(
   ),
 
     # Application title
-    titlePanel(title = div(img(height = 100, src = "logo.jpg")), windowTitle = "EaDania"),
+    titlePanel(
+        title = div(),
+               windowTitle = "EaDania"),
     headerPanel("Passat Web app"),
     div(a("EA-Dania", href = "https://www.eadania.dk", target="_blank")),
     br(),
     # Sidebar with a slider input for number of bins 
     sidebarLayout(position = "left",
         sidebarPanel( "",
+                      
                       selectInput("xaxis", label = "Vælg x-akse:", 
                                   choices = names(passat), selected = "km_per_liter", 
                                   TRUE, multiple = FALSE),
                       selectInput("yaxis", label = "Vælg y-akse:", 
                                   choices = names(passat), multiple = FALSE),
                       helpText(""),
-                      selectInput("data_input", label = "Vælg datasæt:", choices = c("mtcars","faithful","iris"))
-            
-           
+                      selectInput("data_input", label = "Vælg datasæt:", choices = c("mtcars","faithful","iris")),
+                      sliderInput(inputId = "slider_pris", 
+                                  label = "Vælg pris", min = minPrice,
+                                  max = maxPrice,
+                                  value = c(minPrice,maxPrice), step = 1000,
+                                  round = TRUE, ticks = TRUE),
+                      
+                      hr(),
+                      helpText("Input data"),
+                      selectizeInput(inputId = "chaufførid", label = "Chauffør Id",
+                                     choices = c(seq(5000,5099)),
+                                     options = list(
+                                       placeholder = "Vælg dit ID",
+                                       onInitialize = I('function() { this.setValue(""); }')
+                                     )
+                      ),
+                      selectizeInput(inputId = "typedag", label = "Type dag",
+                                     choices = c("Hverdag","Lørdag","Søndag"),
+                                     options = list(
+                                       placeholder = "Vælg dag",
+                                       onInitialize = I('function() { this.setValue(""); }')
+                                     )
+                      ),
+                      radioGroupButtons(inputId = "rute",label = "Vælg rute",
+                                         choices = c("13 - Frydenlund", "13 - Holme Parkvej")),
+                      sliderInput(inputId = "temperatur", label = "Temperatur",
+                                  min = -30, max = 40, value = 0, step = 2),
+                      numericInputIcon(inputId = "batteri", label = "Batteri",
+                                   min = 0, max = 100, step = 0.5, value = NULL,
+                                   help_text = "værdi fra 0-100", icon = icon("percent")
+                      ),
+                      timeInput("tid", label = "Køreplanens starttidspunkt",
+                                seconds = F, value = Sys.time()),
+                      actionButton("tilføj", "Tilføj observation")
+                      
+                      
+                      
+                      
+                      
+                      
             ),
 
         # Show a plot of the generated distribution
         mainPanel("",
                   tabsetPanel(id = "tabs",
+                              tabPanel("Kørselsinformation",
+                                       br(),
+                                       actionButton("indlæs", "Indlæs data"),
+                                       shiny::textOutput("forbrug"),
+                                       br(),
+                                       DTOutput("busTable")),
                               tabPanel("Bil Priser",
-                                       sliderInput(inputId = "slider_pris", 
-                                                   label = "Vælg pris", min = minPrice,
-                                                   max = maxPrice,
-                                                   value = c(minPrice,maxPrice), step = 1000,
-                                                   round = TRUE, ticks = TRUE),
                                        dataTableOutput("price_table")),
                               tabPanel("Bil specs",
                                        selectizeInput(inputId = "add_to_table",
@@ -73,14 +129,67 @@ ui <- fluidPage(
     )
 )
 
+bustable_data <- test
+
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
+  busTableValues <- reactiveValues(df = test)
+  
+  
+  observeEvent(input$tilføj, {
+    
+    show_alert(title = "Observation tilføjet",
+               type = "success")
+    
+    data <- busTableValues$ny
+    
+    newRow <- data.frame(chaufførid = input$chaufførid,
+                         typedag = input$typedag,
+                         rute = input$rute,
+                         temperatur = input$temperatur,
+                         batteri = input$batteri,
+                         tid = format(input$tid, "%H:%M"),
+                         lagToStart = round((round(as.numeric(as.POSIXct(input$tid) - Sys.time()),0) *60) + 
+                           ((as.numeric(as.POSIXct(input$tid) - Sys.time()))%%1),0),
+                         check.names = FALSE)
+    
+    
+    data <- rbind(data, newRow)
+    
+    
+    busTableValues$ny <- data
+    
+    
+    sheet_append("*", newRow, sheet = 1)
+    
+  })
+  
+  df <- eventReactive(input$indlæs, {
+    
+    # read_sheet("https://docs.google.com/spreadsheets/*/edit#gid=0", range = "driver")
+    
+  })
+  
+  output$busTable <- renderDataTable({
+    
+    datatable(df(), style = "bootstrap")
+    
+  })
+  
+  
+  
+  
+  
   
 
-    output$plot1 <- renderPlot({
+        output$plot1 <- renderPlot({
         ggplot(passat,aes_string(x = as.name(input$xaxis), y = as.name(input$yaxis))) +
-            geom_point()
+            geom_point(color = "cyan") + 
+            geom_point(data = df_biler(), color = "white", size = 5, shape = 1) +
+            theme(panel.background = element_rect(color = "grey30", fill = "grey30"))
     })
     
     getdata <- reactive({
